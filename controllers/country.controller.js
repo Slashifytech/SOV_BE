07d321366795ec1cfe6c001a609200f0acb48670
arvindnxtjob <arvind@nxtjob.ai>
2024-10-08 21:05:46 +0530

    FIX: bug fix

--- controllers/country.controller.js
+++ controllers/country.controller.js
@@ -1,4 +1,5 @@
 import { CountryList } from "../models/country.model.js";
+import { Course } from "../models/course.model.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
@@ -30,5 +31,18 @@ const getAllCountries = asyncHandler(async (req, res) => {
     return res.status(200).json(new ApiResponse(200, preferredCountries, "Preferred countries fetched successfully"));
   });
 
-  export {getAllCountries, getPreferredCountries}
+  const getAllCourses = asyncHandler(async (req, res) => {
+    // Fetch all courses from the database
+    const courses = await Course.find({}, { courseName: 1, _id: 0 });
+  
+    // Check if courses exist
+    if (!courses || courses.length === 0) {
+      return res.status(404).json(new ApiResponse(404, {}, "No courses found"));
+    }
+  
+    // Return the courses data
+    return res.status(200).json(new ApiResponse(200, courses, "Courses fetched successfully"));
+  });
+
+  export {getAllCountries, getPreferredCountries, getAllCourses}
   
--- routes/country.routes.js
+++ routes/country.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { getAllCountries, getPreferredCountries } from "../controllers/country.controller.js";
+import { getAllCountries, getAllCourses, getPreferredCountries } from "../controllers/country.controller.js";
+import { verifyJwt } from "../middlewares/auth.middleware.js";
 const router = Router();
 
 router.route("/all").get( getAllCountries);
 router.route("/preferred").get( getPreferredCountries);
+router.route("/courses").get( verifyJwt, getAllCourses);
 
 export default router;
