dfb68ae34a802e4e578032f29ba0059684168226
vinyo-vikas <vikas@getvinyo.com>
2024-10-24 21:53:58 +0530

    FIX: bug fix

--- routes/adminDashboard.routes.js
+++ routes/adminDashboard.routes.js
@@ -7,7 +7,7 @@ router.route("/agent-count").get(verifyAdmin, getTotalAgentsCount);
 router.route("/student-count").get(verifyAdmin, getTotalStudentCount);
 router.route("/change-student-information-status").patch(verifyAdmin, changeStudentInformationStatus);
 router.route("/all/applications").get(verifyAdmin, getAllApplications)
-router.route("/change-application-status").patch(verifyAdmin, changeApplicationStatus)
+router.route("/change-application-status/:institutionId").patch(verifyAdmin, changeApplicationStatus)
 router.route("/all/student-agent-data").get(verifyAdmin, getAllDataAgentStudent)
 router.route("/agent/:id").patch(verifyAdmin, getAllDataAgentStudent)
 router.route("/change-page-status/:id").patch(verifyAdmin, updatePageStatus)
